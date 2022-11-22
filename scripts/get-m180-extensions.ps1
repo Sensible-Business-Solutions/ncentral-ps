469bb1978bbe1c7e7eface97408e5f6cd1ec0ae2
sensible-paulutions <paul.gummerson@sensible.com.au>
2022-11-22 17:06:50 +1100

    Finished M180 export. Code in that module is still messy.

--- scripts/get-m180-extensions.ps1
+++ scripts/get-m180-extensions.ps1
@@ -132,8 +132,8 @@ $NC.DevicePropertyList($DEVICES_SINGLE_MARIST,$null,$null,$null,$true) | Format-
 
 
 
-$all_data = @()
-ForEach ($id in $DEVICES_MULTIPLE_MARIST) {
+#? $all_data = @()
+foreach ($id in $DEVICES_MULTIPLE_MARIST) {
     Write-Host "Device: $($id)"
     $property_id_number = $NC.DevicePropertyID($id, $PROPERTY_NAME)
     Write-Host "Property name '$($PROPERTY_NAME)' for device ID '$($id)' is number $($property_id_number)"
@@ -149,7 +149,7 @@ ForEach ($id in $DEVICES_MULTIPLE_MARIST) {
 
 
 
-$NC.CustomerList()
+#!$NC.CustomerList()
 # NC.
 # NC.DeviceGet(Device IDs, [Bool]ShowProgress)                                  => customerid, deviceid, deviceclass
 # NC.DevicePropertyList(Device IDs, $null, $null, $null, [Bool]ShowProgress)    => DeviceID, "Browser Extension" => {{The data we require}}
@@ -201,6 +201,7 @@ $NC.CustomerList()
 
 
 # 1. Get list of customer(s) filtered by name
+Write-Host "Now getting all matching clients list"
 $customers_filtered = (
     Get-NCCustomerList
     | Where-Object { $_.customername -like $filter_customers }
@@ -216,13 +217,15 @@ $customers_filtered = (
 )
 # DEBUG OUTPUT
 Write-Host "`n`n`nCustomers found matching filter '$($filter_customers)': $($customers_filtered.Count) UNIQUE FOUND: $($customers.Count)"
-$customers_filtered | Select-Object -First 2 | Format-Table
+#$customers_filtered | Select-Object -First 2 | Format-Table
+#$customers | Select-Object -First 2 | Format-Table
 
 
 
 # 2. For each client, get list of endpoint(s) filtered by name
+Write-Host "Now getting client(s) all devices list"
 $devices_filtered_by_name = (
-    Get-NCDeviceList -CustomerIDs ($customers_filtered.customerid)
+    Get-NCDeviceList -CustomerIDs ($customers.Keys)
     | Where-Object { ( `
         $_.longname -like $filter_endpoint_name `
         -or $_.uri -like $filter_endpoint_name `
@@ -245,15 +248,70 @@ $devices_filtered_by_name = (
 #! POSSIBLY ADD AGAIN TO SELECT-OBJECT ABOVE, ORIGINALS HERE: deviceid,sitename,longname,uri,discoveredname,deviceclass,deviceclasslabel,isprobe,licensemode,sourceuri,lastloggedinuser,stillloggedin,osid,supportedos,supportedoslabel,customerid,customername
 # DEBUG OUTPUT
 Write-Host "`n`n`Devices found matching filter '$($filter_endpoint_name)': $($devices_filtered_by_name.Count) UNIQUE FOUND: $($assets.Count)"
-$devices_filtered_by_name | Select-Object -First 2 | Format-Table
+#$devices_filtered_by_name | Select-Object -First 2 | Format-Table
+#$assets | Select-Object -First 2 | Format-Table
 
 
+$JSON_ALL_DATA = @()
+# 3. For each asset, acquire custom properties and save the "Browser Extension" property to the asset object
+Write-Host "Processing job to download custom properties for $($assets.Count) assets. Please wait..."
+foreach ($id in $assets.Keys) {
+    $NC.DevicePropertyList($id, $null, $null, $null, $false)
+    | Select-Object -Property "DeviceID",@{Name="BrowserExtension";Expression={$_."Browser Extension"}}
+    | ForEach-Object {
+        #DEBUG: Write-Host "FOUND CUSTOM PROPERTY DEVICE ($($_.DeviceID)): $($_.BrowserExtension)"
+        $asset = $assets[$_.DeviceID]
+        $asset.JSON_DUMP = $_.BrowserExtension
+        
+        # Add blank entries for Windows endpoints which are not servers
+        if ($asset.JSON_DUMP.length -gt 0) {
+            $FROM_JSON = ($asset.JSON_DUMP | ConvertFrom-Json)
+            Write-Host "DEBUG: Writing normal extension output for device\extension '$($FROM_JSON.Computer)\$($FROM_JSON.ExtensionName)'"
+        }
+        else {
+            if (($asset.Class -ilike "*Windows*") -and ($asset.Class -inotlike "*Server*")) {
+                Write-Host "DEBUG: Creating blank entry for computer name '$($asset.Name)' ID '$($asset.ID)'"
+                $FROM_JSON = [PSCustomObject]@{
+                    ExtensionName = $null
+                    ExtensionTitle = $null
+                    HomepageURL = $null
+                    Description = $asset.Class
+                    Browser = $null
+                    Computer = $asset.Name
+                    User = "NO RECENT CHECK-IN (Device ID: $($asset.ID))"
+                }
+            }
+            else {
+                Write-Host "Skipping device name '$($asset.Name)' ID '$($asset.ID)' of class '$($asset.Class)' due to selection criteria"
+            }
+        }
+        $JSON_ALL_DATA = @($JSON_ALL_DATA; $FROM_JSON)
+    }
+}
+Write-Host "Acquired JSON data. Writing to CSV right now..."
+$JSON_ALL_DATA | Export-Csv -Path ".\scripts\output\m180_6.csv"
 
-# 3. For each device, acquire custom properties and save the "Browser Extension" property to the asset object
-foreach ($asset in $assets)
+#! Attempted to do parallel. Re-visit this later...?
+#$assets.Keys | Foreach-Object -ThrottleLimit 5 -Parallel {
+#    $USING:assets
+#    NC.DevicePropertyList($PSItem, $null, $null, $null, $false)
+#    | Select-Object -Property "DeviceID",@{Name="BrowserExtension";Expression={$_."Browser Extension"}}
+#    | ForEach-Object {
+#        $assets[$_.DeviceID].JSON_DUMP = $_.BrowserExtension
+#    }
+#}
+#$assets | Select-Object -First 2 | Format-Table
 
+#Write-Host "Complete. Now transforming data to appropriate format for export to CSV"
+#$assets_transformed = (
+#    $assets.Keys | ForEach-Object {
+#       [PSCustomObject]$assets[$id]
+#   }
+#)
+#$assets_transformed | Export-Csv -Path ".\scripts\output\m180_3.csv"
 
-
+#Write-Host "COMPLETE. Now exporting to CSV..."
+#$assets | Export-Csv -Path ".\scripts\output\m180.csv"
 
 
 
